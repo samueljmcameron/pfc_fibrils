57e852d5cb82878c69f07be6ea13459aa737f885
Sam Cameron <sam.cameron@dal.ca>
2018-08-10 16:27:49 -0300

    just started modifying the code to correctly account for the density fluctuation term I neglected so far. I am currently editing the energy.c file, to re-order variables and also add in new terms from omega!=0

--- src/energy.c
+++ src/energy.c
@@ -3,9 +3,9 @@
 #include "nrutil.h"
 #include "headerfile.h"
 
-void compute_rf2233b(double K33, double Lambda,double eta, 
-		     double d0, double L,double *r,
-		     double **y, double *rf_,int mpt)
+void compute_rf2233b1(double K33, double Lambda,double d0,
+		      double L,double eta,double delta,
+		      double *r,double **y, double *rf_,int mpt)
 {
   int i;
   double siny, sin2y,cosy;
@@ -19,8 +19,10 @@ void compute_rf2233b(double K33, double Lambda,double eta,
     rf_[i] = r[i]*(-(y[2][i]+0.5*sin2y/r[i])
 		   +0.5*(y[2][i]+0.5*sin2y/r[i])
 		   *(y[2][i]+0.5*sin2y/r[i])
-		   +0.5*K33*siny*siny*siny*siny/(r[i]*r[i])
-		   +Lambda/4.0*(1+sin(2*eta*L)/(2*eta*L))
+		   +0.5*K33*siny*siny*siny
+		   *siny/(r[i]*r[i])
+		   +Lambda*delta*delta/4.0*
+		   (1+sin(2*eta*L)/(2*eta*L))
 		   *(4*M_PI*M_PI/(d0*d0*cosy*cosy)-eta*eta)
 		   *(4*M_PI*M_PI/(d0*d0*cosy*cosy)-eta*eta));
   }
@@ -64,13 +66,16 @@ void compute_integrand2(double Lambda,double eta, double d0,
   return;
 }
 
-double E_R(double R, double k24, double L, double gamma_s,
-	   double gamma_t,double *r,double **y, 
-	   double integration_2233b,int mpt)
+double E_R(double k24,double omega,double R,double L,double eta,
+	   double delta,double gamma_s,double gamma_t,double *r,
+	   double **y,double integration_2233b1,int mpt)
 {
 
   double E;
-  E = 2.0/(R*R)*integration_2233b; // first calculate bulk energy per unit length
+  E = 2.0/(R*R)*integration_2233b1; // first calculate bulk energy per unit length
+  // add density fluctuations term
+  E = E+omega*0.5*(0.75*delta*delta-1+sin(2*eta*L)/(2*eta*L)*(delta*delta-1)
+		   +delta*delta*sin(4*eta*L)/(8*eta*L))
   // add surface term tension terms
   E = E+1.0/R*(-(1+k24)*(sin(y[1][mpt])*sin(y[1][mpt]))/R+2.0*gamma_s);  
   E = E+2*gamma_t/L;
@@ -78,9 +83,9 @@ double E_R(double R, double k24, double L, double gamma_s,
   return E;
 }
 
-double derivEdR(double R, double K33, double k24,double Lambda,
-		double eta, double d0, double L, double gamma_s,
-		double *r,double **y, double integration_2233b,
+double derivEdR(double K33, double k24,double Lambda,double d0, 
+		double R,double L,double eta,double delta,double gamma_s,
+		double *r,double **y, double integration_2233b1,
 		int mpt)
 {
   double ans;
@@ -88,13 +93,13 @@ double derivEdR(double R, double K33, double k24,double Lambda,
   double siny = sin(y[1][mpt]);
   double cosy = cos(y[1][mpt]);
 
-  ans = -4.0/(R*R*R)*integration_2233b;
+  ans = -4.0/(R*R*R)*integration_2233b1;
 
   ans += 2.0/R*(-(y[2][mpt]+0.5*sin2y/R)+0.5*(y[2][mpt]+0.5*sin2y/R)
   		*(y[2][mpt]+0.5*sin2y/R)
   		+0.5*K33*(siny*siny*siny*siny)/(R*R));
 
-  ans += (0.5*Lambda/R*(1+sin(2*eta*L)/(2*eta*L))
+  ans += (0.5*Lambda*delta*delta/R*(1+sin(2*eta*L)/(2*eta*L))
 	  *(4*M_PI*M_PI/(d0*d0*cosy*cosy)-eta*eta)
 	  *(4*M_PI*M_PI/(d0*d0*cosy*cosy)-eta*eta));
   
@@ -102,6 +107,8 @@ double derivEdR(double R, double K33, double k24,double Lambda,
 
   return ans;
 }
+
+// done up to here 2018-08-10
 
 double derivEdeta(double R,double Lambda,double eta, 
 		  double d0, double L, double *r,double **y,
@@ -136,10 +143,10 @@ void energy_stuff(double *E, double *dEdR,double *dEdeta, double *dEdL,
 		  double gamma_t, double *r, double **y, double *rf_,
 		  double *integrand1, double *integrand2,int mpt)
 {
-  double integration_2233b,integration1,integration2;
+  double integration_2233b1,integration1,integration2;
 
-  compute_rf2233b(K33,Lambda,eta,d0,L,r,y,rf_,mpt);
-  integration_2233b = qromb(r,rf_,mpt);
+  compute_rf2233b1(K33,Lambda,eta,d0,L,r,y,rf_,mpt);
+  integration_2233b1 = qromb(r,rf_,mpt);
 
   compute_integrand1(Lambda,eta,d0,L,r,y,integrand1,mpt);
   integration1 = qromb(r,integrand1,mpt);
@@ -148,10 +155,10 @@ void energy_stuff(double *E, double *dEdR,double *dEdeta, double *dEdL,
   integration2 = qromb(r,integrand2,mpt);
 
 
-  *E = E_R(R,k24,L,gamma_s,gamma_t,r,y,integration_2233b,mpt);
+  *E = E_R(R,k24,L,gamma_s,gamma_t,r,y,integration_2233b1,mpt);
 
   *dEdR = derivEdR(R,K33,k24,Lambda,eta,d0,L,gamma_s,r,y,
-		   integration_2233b,mpt);
+		   integration_2233b1,mpt);
 
   *dEdeta = derivEdeta(R,Lambda,eta,d0,L,r,y,integration1,
 		       integration2);
