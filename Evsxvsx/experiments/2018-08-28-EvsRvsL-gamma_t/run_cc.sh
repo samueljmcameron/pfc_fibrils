c48e2b22e65aa8b86a05f2d6d759b36a142e57cd
Sam Cameron <samuel.j.m.cameron@gmail.com>
2018-08-28 11:52:20 -0700

    ran the first (actually correct) EvsLvsR experiment.

--- Evsxvsx/experiments/2018-08-28-EvsRvsL-gamma_t/run_cc.sh
+++ Evsxvsx/experiments/2018-08-28-EvsRvsL-gamma_t/run_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=8:00:00
 #SBATCH --account=def-arutenbe
-#SBATCH --array 1-8
+##SBATCH --array 1-2
 
 module restore standard_modules
 module load python/2.7
