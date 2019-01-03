18792ed3983b9cc7fe8d7c0dd6c521cae69ab133
Sam Cameron <samuel.j.m.cameron@gmail.com>
2019-01-03 07:58:14 -0800

    adding results of 2019-01-02 experiments to look at a grid of k24 gamma values in omega Lambda space.

--- gradient_descent/experiments/2019-01-02/omega-Lambda-space/runfirst.h
+++ gradient_descent/experiments/2019-01-02/omega-Lambda-space/runfirst.h
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=00:15:00
 #SBATCH --account=def-arutenbe
-#SBATCH --output=slurmoutput/run_%j.out
+#SBATCH --output=firstrunslurmoutput/run_%j.out
 
 module restore standard_modules
 
