639884be84739b638a3d32f3bfb69f3cf2101a05
Sam Cameron <samuel.j.m.cameron@gmail.com>
2019-01-10 17:35:40 -0400

    fixing small issue with code, resending experiments for omega from 0.01 to 10.0 and Lambda from 0.1 to 1000 to compute canada.  I am also sending experiments of gamma k24 space.

--- gradient_descent/experiments/2019-01-10/gamma-k24-space-K33is20/run_cc.h
+++ gradient_descent/experiments/2019-01-10/gamma-k24-space-K33is20/run_cc.h
@@ -2,7 +2,7 @@
 #SBATCH --time=20:00:00
 #SBATCH --account=def-arutenbe
 #SBATCH --output=slurmoutput/run_%A_%a.out
-#SBATCH --array=1-100
+#SBATCH --array=0-100
 
 module restore standard_modules
 
