still trying to figure out what is goind wrong with my dEdpsip_R calculation!

piecewise_linear/experiments/2019-02-25/testing_Evspsip_R/testing_Evspsip_R.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     axarr[0].scatter(psip_Rs[i_min],Es[i_min])
     axarr[0].set_ylabel(r"$E(psip_R)$")
     axarr[1].plot(psip_Rs,dEdpsip_Rs,'.')
-    #axarr[1].plot(psip_Rs,np.abs(dEdpsip_Rs),'--')
+    axarr[1].plot(psip_Rs,np.abs(dEdpsip_Rs),'--')
     axarr[1].scatter(psip_Rs[j_zero1],dEdpsip_Rs[j_zero1])
     axarr[1].set_ylabel(r"$dE/d\psi_R^{\prime}$")
     axarr[1].set_xlabel(r"$\psi_R^{\prime}$")