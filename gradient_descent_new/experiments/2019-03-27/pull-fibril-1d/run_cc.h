d6d12acc5f4fb0e55ab6c539ea8eb5892c7fa862
Sam Cameron <samuel.j.m.cameron@gmail.com>
2019-03-29 05:38:40 -0700

    results for mechanical response of fibrils at gamma,k24 = 0.04,0 and 0.12,1 are in.

--- gradient_descent_new/experiments/2019-03-27/pull-fibril-1d/run_cc.h
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=04:00:00
-#SBATCH --account=def-arutenbe
-#SBATCH --array=1,10-1000:10
-
-module restore standard_modules
-
-
-python pulling.py 0.04 0.1 $1 $SLURM_ARRAY_TASK_ID
